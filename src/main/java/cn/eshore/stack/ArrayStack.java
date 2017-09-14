a987d3c72651736af8230b690ef1038569b2ddd4
zengly <291893221@qq.com>
2017-09-14 10:54:45 +0800

    用数组实现堆栈数据结构

--- src/main/java/cn/eshore/stack/ArrayStack.java
+++ src/main/java/cn/eshore/stack/ArrayStack.java
@@ -1,35 +1,75 @@
 package cn.eshore.stack;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+@SuppressWarnings("unchecked")
 public class ArrayStack<T> implements Stack<T> {
 
+	// 用数组进行存储
+	private T[] array;
+
+	// 游标
+	private int index = 0;
+
+	// 构造函数
+	public ArrayStack() {
+		super();
+		array = (T[]) new Object[3];
+	}
+
+	public int size() {
+		return index;
+	}
+
 	public void push(T t) {
-		
-		
+		// 判断数组是否越界
+		if (index >= array.length) {
+			// 数组扩容
+			array = Arrays.copyOf(array, index + index / 2);
+		}
+
+		array[index++] = t;
 	}
 
 	public T pop() {
-		
-		return null;
+		T t = array[--index];
+		array[index] = null;
+		return t;
 	}
 
 	public T peek() {
-		
-		return null;
+		return array[index - 1];
 	}
 
 	public Boolean isEmpty() {
-		
-		return null;
+		return index == 0;
 	}
 
 	public int search(T t) {
-		
-		return 0;
+		for (int i = 0; i < index; i++) {
+			T ti = array[i];
+			if (t.equals(ti)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	public void reverse() {
-		
-		
+		array = Arrays.copyOf(array, index);
+		Collections.reverse(Arrays.asList(array));
 	}
 
+	@Override
+	public String toString() {
+		String rs = "";
+		for (int i = 0; i < index; i++) {
+			T t = array[i];
+			rs += ", " + t.toString();
+		}
+		rs = rs.replaceFirst(",", "[");
+		rs += "]";
+		return rs;
+	}
 }
---
+++ src/main/java/cn/eshore/stack/ArrayStackTest.java
@@ -0,0 +1,22 @@
+package cn.eshore.stack;
+
+public class ArrayStackTest {
+	public static void main(String[] args) {
+		ArrayStack<String> arrayStack = new ArrayStack<String>();
+		arrayStack.push("A");
+		arrayStack.push("B");
+		arrayStack.push("C");
+		arrayStack.push("D");
+		arrayStack.push("E");
+		arrayStack.push("F");
+		System.out.println(arrayStack.isEmpty());
+		System.out.println(arrayStack);
+		System.out.println(arrayStack.pop());
+		System.out.println(arrayStack);
+		System.out.println(arrayStack.peek());
+		System.out.println(arrayStack);
+		arrayStack.reverse();
+		System.out.println(arrayStack);
+		System.out.println(arrayStack.search("C"));
+	}
+}
